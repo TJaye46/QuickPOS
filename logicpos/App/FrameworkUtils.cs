b385d632559a5f7d43cb404a8ef056389c6cad0c
TJaye46 <75900705+TJaye46@users.noreply.github.com>
2022-09-29 16:38:07 +0300

    Update FrameworkUtils.cs

--- logicpos/App/FrameworkUtils.cs
+++ logicpos/App/FrameworkUtils.cs
@@ -1,6 +1,6 @@
-﻿namespace logicpos.App
+﻿namespace Quickpos.App
 {
-    public class FrameworkUtils  : logicpos.financial.library.App.FrameworkUtils
+    public class FrameworkUtils  : quickpos.financial.library.App.FrameworkUtils
     {
         //Log4Net
         private static log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
